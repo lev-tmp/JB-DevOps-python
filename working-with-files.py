b3ec6b6dbfdfffb586400fa7798fcf2013c56c50
levep-temp <le.vepstein@gmail.com>
2017-11-10 02:52:12 +0200

    demo working with file finished

--- working-with-files.py
+++ working-with-files.py
@@ -11,16 +11,14 @@ print(os.getcwd())
 
 # print(f.name)
 # print(f.mode)
-with open('test.txt','r') as f:
-    size_to_read = 10
+# with open('test.txt','r') as f:
 
-    f_contents = f.read(size_to_read)
-    print(f_contents, end='')
-
-    f.seek(0)
-
-    f_contents = f.read(size_to_read)
-    print(f_contents, end='')
+    # size_to_read = 10
+    # f_contents = f.read(size_to_read)
+    # print(f_contents, end='')
+    # f.seek(0)
+    # f_contents = f.read(size_to_read)
+    # print(f_contents, end='')
 
     # while len(f_contents) > 0:
     #     print(f_contents, end='*')
@@ -38,5 +36,12 @@ with open('test.txt','r') as f:
     # for line in f:
     #     print(line, end='')
 
-
+with open('screen.png','rb') as rf:
+    with open('screen_copy.png','wb') as wf:
+        for line in rf:
+            wf.write(line)
+    # pass for only cration file analogy to touch
+    # f.write('Test')
+    # f.seek(0) not many uses on w mode show
+    # f.write('Test')
 
