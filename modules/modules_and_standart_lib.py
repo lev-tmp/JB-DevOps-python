144f9b8c5bba46b253d86b28e1443ba46572ca88
lev-tmp <le.vepstein@gmail.com>
2017-11-12 13:53:37 +0200

    started modules explanation

---
+++ modules/modules_and_standart_lib.py
@@ -0,0 +1,14 @@
+# import my_module
+#
+#
+# list_1 = ['History', 'Math', 'Physics', 'CompSci']
+#
+# index = my_module.find_index(list_1,'Math')
+# print(index)
+
+
+# to show : import my_module as mm, from my_module find_index, from my module import find_index as fi, test
+
+import sys
+
+print(sys.path)
