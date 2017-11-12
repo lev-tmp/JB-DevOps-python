f5ae6944e6703bdd02ca76305c44ed46246144b6
lev-tmp <le.vepstein@gmail.com>
2017-11-12 12:54:32 +0200

    loops explanation finished

--- loops_and_iterations/loops.py
+++ loops_and_iterations/loops.py
@@ -1,18 +1,39 @@
 a = [1,2,3,4,5,6]
 
-for num in range(1,7):
-    for latter in 'abc':
-        print(num,latter)
-str = """
-Hello big
-and little
-and grimme and groovy"""
-
-print(f"{a.pop()}")
+## 1 for loop example
+# for num in a:
+#     print(num)
 
 
+## break , continue
+# for num in a:
+#     if num == 4:
+#         print('Found')
+#         break
+#     print(num)
+
+# for num in a:
+#     if num == 4:
+#         print('Found')
+#         continue
+#     print(num)
+
+##  for in for, and range function
+# for num in range(1,7):
+#     for latter in 'abc':
+#         print(num,latter)
+
+
+## while loop example
 # x = 0
 #
 # while x < 10:
+#     x += 1
+#     print(x)
+
+## while infinity
+# x = 0
+#
+# while True:
 #     x += 1
 #     print(x)
