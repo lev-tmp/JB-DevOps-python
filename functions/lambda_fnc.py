62fe5e9379c66e110d6654ed4c7fb69d4225b7f5
lev-tmp <le.vepstein@gmail.com>
2017-11-17 11:40:03 +0200

    added lambda function tutorial

---
+++ functions/lambda_fnc.py
@@ -0,0 +1,25 @@
+# lambda expretion or lambda function
+# anonymous funtion - function that don't have name bounce to it
+
+# regular function
+def p(num):
+    return num ** 2
+print(p(4))
+print(type(p))
+
+# lambda function general syntax : lambda <var>: <expretion>
+y = lambda x: x ** 2
+print(y(4))
+print(y)
+
+## Why is usefull, for example :
+def h(n):
+    return lambda x: (x + n) ** 2
+
+c = h(3)
+b = h(0)
+print(c)
+print(b)
+
+print (b(2))
+print(c(2))
