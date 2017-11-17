2524b483cf4ac22f436dbe4c3710bcec21a3d668
lev-tmp <le.vepstein@gmail.com>
2017-11-17 10:07:54 +0200

    modules part finished

--- modules/os-descoverymodule.py
+++ modules/os-descoverymodule.py
@@ -3,39 +3,50 @@ from datetime import datetime
 # print(dir(os))
 
 print(os.getcwd())
-
 os.chdir('/Users/lev/Desktop')
+print(os.getcwd())
 
+## directories :
+# print(os.listdir())
 # os.mkdir('os-demo-1')
 # os.makedirs('os-demo-1/Sub-dir-1')
 # os.removedirs('os-demo-1/Sub-dir-1')
 # os.rename('python2- lev.pptx', 'python2-lev.pptx')
 
+## files stats
+# print(os.stat('python2-lev.pptx'))
+# print(os.stat('python2-lev.pptx').st_size)
 # print(os.stat('python2-lev.pptx').st_mtime)
-
+# mod_time = os.stat('python2-lev.pptx').st_mtime
+# print(datetime.fromtimestamp(mod_time))
 # mod_time = os.stat('python2-lev.pptx').st_mtime
 # print(datetime.fromtimestamp(mod_time))
 
-# print(os.getcwd())
-# print(os.listdir())
 
+## Walking throught directories and files :
 # for dirpath, dirname, filename in os.walk('/Users/lev/Desktop'):
 #     print('Current path',dirpath)
 #     print('Directories:', dirname)
 #     print('Files:', filename)
 #     print()
 
+## Access to env variebles :
 # print(os.environ.get('HOME'))
 
+# path to file withou join
+# file_path = os.environ.get('HOME') + 'test.txt'
+# print(file_path)
+
+# path to file with join
 # file_path = os.path.join(os.environ.get('HOME'),'test.txt')
 # print(file_path)
 
+## some usefull os.path :
 # print(os.path.basename('/tmp/test.txt'))
 # print(os.path.dirname('/tmp/test.txt'))
 # print(os.path.split('/tmp/test.txt'))
 # print(os.path.exists('/tmp/test.txt'))
-# os.path.isdir() os.path.isfile()
+# print(os.path.isdir('/Users'))
+# os.path.isfile()
+# split file and extention :
 # print(os.path.splitext('/tmp/test.txt'))
-
-# print(dir(os.path))
-print(dir(int))
