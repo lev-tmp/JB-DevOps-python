c7bc3b17cdfcc11fc088159eeb100fdc659f7549
lev-tmp <le.vepstein@gmail.com>
2017-11-19 07:27:33 +0200

    lambda fixed

--- functions/lambda_fnc.py
+++ functions/lambda_fnc.py
@@ -2,13 +2,12 @@
 # anonymous funtion - function that don't have name bounce to it
 
 # regular function
-def p(num):
-    return num ** 2
-print(p(4))
-print(type(p))
+# def p(num):
+#     return num ** 2
+# print(p(4))
+# print(type(p))
 
-# lambda function general syntax : lambda <var>: <expretion>
-# y = lambda x: x ** 2
+# lambda function general syntax :  y = lambda x: x ** 2
 # print(y(4))
 # print(y)
 
